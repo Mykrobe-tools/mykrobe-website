082373f9157639f58d69643f62275f909fe71b91
Simon Heys <si@simonheys.com>
2014-04-21 06:42:22 +0100

    styling

--- website/wp-content/themes/mykrobe/template-product.php
+++ website/wp-content/themes/mykrobe/template-product.php
@@ -5,12 +5,16 @@
 ?>
 <?php get_header(); ?>
 <?php
-		$image_id = get_field('image');
-		$description = get_field('description');
-		$image_attributes = wp_get_attachment_image_src($image_id, 'medium');
-		$url = $image_attributes[0];
-		$width = $image_attributes[1];
-		$height = $image_attributes[2];
+require_once 'php/Products.php';
+$products = new Products;
+
+$image_id = get_field('image');
+$description = get_field('description');
+$image_attributes = wp_get_attachment_image_src($image_id, 'medium');
+$url = $image_attributes[0];
+$width = $image_attributes[1];
+$height = $image_attributes[2];
+
 ?>
 	<div class="product-overview container">
 		<div class="row">
@@ -29,38 +33,41 @@
 	</div>
 	<div class="product-species container">
 <?php
-if( get_field('species') ) {
-	$counter = 0;
-	$row_open = false;
-	while( has_sub_field('species') ) {
-		$title = get_sub_field('title');
-		$description = get_sub_field('description');
-		$small_print = get_sub_field('small_print');
-		$image_id = get_sub_field('logo');
-		$image_attributes = wp_get_attachment_image_src($image_id, 'medium');
-		$url = $image_attributes[0];
-		$width = $image_attributes[1];
-		$height = $image_attributes[2];
-?>
+
+$all_species = $products->getProductSpecies($post);
+
+if ( count($all_species) > 1 ) {
+	// draw tabs here
+}
+
+foreach($all_species as $species) {
+	$title = $species['title'];
+	$description = $species['description'];
+	$small_print = $species['small_print'];
+	$image_id = $species['image_id'];
+	$permalink = $species['permalink'];
+	$anchor = $species['anchor'];
+	$image_attributes = wp_get_attachment_image_src($image_id, 'medium');
+	$url = $image_attributes[0];
+echo '
 		<div class="product-single-species row">
+			<a name="'.$anchor.'" id="'.$anchor.'"></a>
 			<div class="sixcol product-single-species-overview">
-				<?php /* echo $title; */ ?>
-				<img class="product-single-species-overview-logo" src="<?php echo $url; ?>">
+				<img class="product-single-species-overview-logo" src="'.$url.'">
 				<article>
-					<?php echo $description; ?>
+					'.$description.'
 				</article>
 				<a class="button download">Download</a>
 			</div>
 			<div class="threecol last product-single-species-small-print">
 				<h3>Technical specs</h3>
 				<article>
-					<?php echo $small_print; ?>
+					'.$small_print.'
 				</article>
 				<a class="button download secondary">Download</a>
 			</div>
 		</div>
-<?php
-	}
+';
 }
 ?>
 	</div>
