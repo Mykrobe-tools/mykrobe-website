99d71909ff74414063ebcb3bebe4f4671e9f8506
Simon Heys <si@simonheys.com>
2014-04-21 13:26:19 +0100

    styling

--- website/wp-content/themes/mykrobe/template-home.php
+++ website/wp-content/themes/mykrobe/template-home.php
@@ -4,81 +4,19 @@
  */
 ?>
 <?php get_header(); ?>
-	<div class="what-is-helix container">
-		<div class="row row-statement" id="step-1" data-title="">
-			<div class="content-wrapper">
-				<div class="twelvecol last statement">
 <?php
-$statement_field = get_field('statement');
-if ( $statement_field ) {
-	$number_of_statements = count($statement_field);
-	if (!isset($_SESSION['helixcentre_statement_counter'])) {
-		$_SESSION['helixcentre_statement_counter'] = 0;
-	}
-	else {
-		$_SESSION['helixcentre_statement_counter'] = ($_SESSION['helixcentre_statement_counter'] + 1) % $number_of_statements;
-	}
-	// echo $_SESSION['helixcentre_statement_counter'];
-
-	// echo $number_of_statements;
-
-	$statement = $statement_field[$_SESSION['helixcentre_statement_counter']];
-	echo $statement['statement'];
-	/*
-
-	echo '
-					<ul>
-	';
-	while( has_sub_field('statement') ) {
-		$statement = get_sub_field('statement');
-		echo '
-						<li>'.$statement.'</li>
-		';
-	}
-	echo '
-					</ul>
-	';
-
-	*/
-}
+$description = get_field('description');
+$link_page = get_field('link');
+$permalink = get_permalink($link_page->ID);
 ?>
-				</div>
+	<div class="promo-container">
+		<div class="promo">
+			<div class="promo-background"></div>
+			<div class="promo-content">
+				<h1 class="logo"><?php bloginfo('name');?></h1>
+				<?php echo $description; ?>
+				<a href="<?php echo $permalink; ?>" class="button learn">Learn more</a>
 			</div>
 		</div>
-<?php
-
-if ( get_field('q-and-a') ) {
-	$index = 2;
-
-	while( has_sub_field('q-and-a') ) {
-		$question = get_sub_field('question');
-		$question = trim(strip_tags($question));
-		$answer = get_sub_field('answer');
-		echo '
-		<div class="row row-q-and-a" id="step-'.$index.'" data-title="'.$question.'">
-			<div class="content-wrapper">
-				<div class="fourcol question">'.$question.'</div>
-				<div class="eightcol last answer">'.$answer.'</div>
-			</div>
-		</div>
-		';
-		$index++;
-	}
-}
-$question = get_field('final-question');
-$question = trim(strip_tags($question));
-echo '
-	<div class="row row-final" id="step-'.$index.'" data-title="'.$question.'">
-		<div class="content-wrapper">
-			<div class="fourcol question">'.$question.'</div>
-			<div class="eightcol last answer answer-last">
-				'.get_field('mailing-list-body', 'option').'
-				'.get_field('mailing-list-html', 'option').'
-				'.get_field('twitter-html', 'option').'
-			</div>
-		</div>
-	</div>
-';
-?>
 	</div>
 <?php get_footer(); ?>
